4d10247e1de6f017e1602abaa1c7d86eb3145969
Anthony Benavente <anthony@admissionpros.com>
2026-06-08 21:08:23 -0400

    added vec to track users in join order

--- src/room.rs
+++ src/room.rs
@@ -8,6 +8,7 @@ use anyhow::{Result, bail};
 #[derive(Default)]
 pub struct Room {
     pub users: HashMap<u64, String>,
+    pub ordered_users: Vec<String>
 }
 
 impl Room {
@@ -18,14 +19,25 @@ impl Room {
     pub fn join(&mut self, id: u64, nick: &str) -> Result<()> {
         match self.users.entry(id) {
             Occupied(_) => bail!("user already in room"),
-            Vacant(e) => e.insert(nick.to_string()),
+            Vacant(e) => {
+                e.insert(nick.to_string());
+                self.ordered_users.push(nick.to_string());
+            }
         };
         Ok(())
     }
 
     pub fn leave(&mut self, id: u64) {
         if let Occupied(entry) = self.users.entry(id) {
-            entry.remove();
+            let nick = entry.remove();
+
+            // choosing to iterate instead of using .retain so we can exit the loop sooner
+            for i in (0..self.ordered_users.len()).rev() {
+                if self.ordered_users[i] == nick {
+                    self.ordered_users.remove(i);
+                    break;
+                }
+            }
         }
     }
 }
